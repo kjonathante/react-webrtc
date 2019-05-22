0d69e769c2c7b80e76acd232dbad1d4cb49eff3f
Kit Jonathan Te <kjonathante@gmail.com>
2019-05-21 23:53:57 -0700

    Translated code from webrtc sample code step-01

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+// On this codelab, you will be streaming only video (video: true).
+const mediaStreamConstraints = {
+  video: true,
+};
+
+// Handles error by logging a message to the console with the error message.
+function handleLocalMediaStreamError(error) {
+  console.log('navigator.getUserMedia error: ', error);
+}
+
+const App = () => {
+  let ref = React.createRef(); // Video element where stream will be placed.
+
+  // Local stream that will be reproduced on the video.
+  let localStream;
+  
+  const handleClick = () => {
+    ref.current.setAttribute("autoplay", "");
+    ref.current.setAttribute("playsinline", "");
+    navigator.mediaDevices.getUserMedia(mediaStreamConstraints)
+      .then( mediaStream => { // Handles success by adding the MediaStream to the video element.
+        localStream = mediaStream
+        ref.current.srcObject = mediaStream
+      })
+      .catch(handleLocalMediaStreamError);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <video ref={ref} style={{width: "320px", maxWidth: "100%" }}></video>
+      <button onClick={handleClick}>Test</button>
+    </>
+  )
 }
 
 export default App;
